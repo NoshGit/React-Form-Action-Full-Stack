fe4c66890f0577f2c43002dcc8bc40e6f6a80e97
Noshir Patel <noshirpatel86@gmail.com>
2026-02-09 13:15:17 +0530

    Implement useActionState hook to handle form

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,8 +1,24 @@
+import { useActionState } from 'react';
+
 export function NewOpinion() {
+  const handleFormAction = (prevState, formData) => {
+    const form = {
+      userName: formData.get('userName'),
+      title: formData.get('title'),
+      body: formData.get('body'),
+    };
+
+    return { errors: null };
+  };
+
+  const [formState, formAction] = useActionState(handleFormAction, {
+    errors: null,
+  });
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
